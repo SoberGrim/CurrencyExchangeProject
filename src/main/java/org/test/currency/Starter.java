dbbbf3360ea7dd2ae837561462deea4cba280fc2
SoberGrim <sobergrim@gmail.com>
2022-02-02 11:58:22 +0300

    v 1.22

--- src/main/java/org/test/currency/Starter.java
+++ src/main/java/org/test/currency/Starter.java
@@ -19,16 +19,22 @@ public class Starter {
 
     public static void main(String[] args) {
 
+
+        //Валюта живет своей жизнью и ей никто не требуется. В режиме verbose = true кроме курса сообщает доп. даные
         Currency usd = new Currency("USD", 78, '$', 11000, ANSI_GREEN, false);
         Currency eur = new Currency("EUR", 98, '€', 3000, ANSI_PURPLE, false);
 
-       // ScheduledExecutorService service = Executors.newScheduledThreadPool(2);
-       // service.scheduleAtFixedRate(() -> System.out.println(usd), 0, 1000, TimeUnit.MILLISECONDS);
-       // service.scheduleAtFixedRate(() -> System.out.println(eur), 0, 100, TimeUnit.MILLISECONDS);
+        //Экзекьюторы которые показывают текущий курс валюты
+        // ScheduledExecutorService service = Executors.newScheduledThreadPool(2);
+        // service.scheduleAtFixedRate(() -> System.out.println(usd), 0, 1000, TimeUnit.MILLISECONDS);
+        // service.scheduleAtFixedRate(() -> System.out.println(eur), 0, 1000, TimeUnit.MILLISECONDS);
 
+        //"Биржа". Мониторит курс валют и оповещает о его изменнеиях подписчиков
+        //В режиме verbose = true дополнительно сообщает об изменениях курсов в консоль
         PriceMonitor stockExchange = new PriceThrottler("Moscow Exchange",false, eur, usd);
 
-       // new PriceConsumer("Times",31).subscribe(stockExchange);
+        //клиенты биржи. ПОлучают от биржи курс, при получении "думают" incomingDelay секунд, после печатают курс в консоль
+        // new PriceConsumer("Times",31).subscribe(stockExchange);
         new PriceConsumer("CNN",7).subscribe(stockExchange);
 
     }
